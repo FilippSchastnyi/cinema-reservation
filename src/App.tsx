7f9381b6efd854a151852a1a96ab270a2fa3015b
Pilip Shchasny <filippshchasny@gmail.com>
2023-01-24 21:29:54 +0100

    CR-pshchasny-created-auth-context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { AuthContext, authContext } from '@src/contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import './css/_index.scss';
 
 const App = () => (
-  <AppRoutes
-    isAuthorized={false}
-  />
+  <AuthContext.Provider value={authContext}>
+    <AppRoutes
+      isAuthorized={false}
+    />
+  </AuthContext.Provider>
 );
 
 export default App;
