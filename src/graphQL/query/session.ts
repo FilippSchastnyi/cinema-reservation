bd88ad84ca85e373da685575b036065933181a1b
Pilip Shchasny <filippshchasny@gmail.com>
2023-02-24 18:29:34 +0100

    CR-pshchasny-work-on-the-booking-data-fetch-and-types

--- src/graphQL/query/session.ts
+++ src/graphQL/query/session.ts
@@ -1,10 +1,23 @@
 import gql from 'graphql-tag'
 
 export const GET_SESSION_DETAILS = gql`
-    query GetSessionDetails($id: ID){
-        getOneSession(id: $id){
-            film
-            
+    query GetOneSession($getOneSessionId: ID!) {
+        sessionData:getOneSession(id: $getOneSessionId) {
+            hallName
+            hall {
+                rowNumber
+                seats {
+                    seatNumber
+                    status
+                    isBusy
+                }
+
+            }
+            showTime
+            booking {
+                row
+                seats
+            }
         }
-    } 
+    }
 `
